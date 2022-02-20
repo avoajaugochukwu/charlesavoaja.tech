8dfe0bceac3e456dab6eb47de5f5dfefd48211c3
Ugochukwu Charles <ugochukwucharles@Tech-Ugo.local>
2022-02-20 19:13:01 +0100

    Added loading page

--- screens/Blog/Details/index.js
+++ screens/Blog/Details/index.js
@@ -2,23 +2,30 @@ import React from "react";
 import { MDXRemote } from "next-mdx-remote";
 
 const index = ({ post }) => {
-  let mdxSource, meta = null
+  let mdxSource,
+    meta = null;
   if (post) {
-    mdxSource = post.mdxSource
-    meta = post.meta  
+    mdxSource = post.mdxSource;
+    meta = post.meta;
   }
-  
 
   return (
-    <article className="page sans">
-      <header>
-        <h1 className="page-title">{meta.title}</h1>
-      </header>
-      <div className="page-body">
-        {mdxSource && <MDXRemote {...mdxSource} />}
-        
-      </div>
-    </article>
+    <>
+      {mdxSource && meta ? (
+        <article className="page sans">
+          <header>
+            <h1 className="page-title">{meta.title}</h1>
+          </header>
+          <div className="page-body">
+            {mdxSource && <MDXRemote {...mdxSource} />}
+          </div>
+        </article>
+      ) : (
+        <div className="flex h-screen justify-center items-center">
+          <h1 className="text-[#7510f7] text-4xl font-bold">Loading</h1>
+        </div>
+      )}
+    </>
   );
 };
 
